1afb8240e5d4be03d3ecc4abe3c1400c3dd4d11b
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 10:07:10 +1000

    🥜 Example of importing Semantic component

---
+++ elpis-gui/src/components/Example.js
@@ -0,0 +1,8 @@
+// https://react.semantic-ui.com/
+
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ExampleButton = () => <Button>Click Here</Button>
+
+export default ExampleButton
