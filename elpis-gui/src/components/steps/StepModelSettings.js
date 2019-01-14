03a4b0cf71606a322039a4770acb9db2021d99b8
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 12:45:06 +1000

    📋 Added all steps except error handling

---
+++ elpis-gui/src/components/steps/StepModelSettings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepModelSettings extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return <div></div>;
+    }
+}
---
+++ elpis-gui/src/components/steps/StepTrainingSuccess.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepTrainingSuccess extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        return <div></div>;
+    }
+}
