be7a0201d309c34a00f0c5e22d63111cc3d640fb
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 14:58:38 +1000

    📂 Added general redirects

--- elpis-gui/src/components/App.js
+++ elpis-gui/src/components/App.js
@@ -18,58 +18,92 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // step: this.makeStepWelcome(),
-      step: this.makeStepAddData()
+      step: this.makeStepWelcome(),
+      // step: this.makeStepAddData()
     };
   }
 
+  stepProps = {
+    toStepWelcome: () => this.setState({step: this.makeStepWelcome()}),
+    toStepBuildPronunciationDictionary: () => this.setState({step: this.makeStepBuildPronunciationDictionary()}),
+    toStepDataPreperation: () => this.setState({step: this.makeStepDataPreperation()}),
+    toStepNaming: () => this.setState({step: this.makeStepNaming()}),
+    toStepAddData: () => this.setState({step: this.makeStepAddData()}),
+    toStepNewTranscription: () => this.setState({step: this.makeStepNewTranscription()}),
+    toStepTrainingSuccess: () => this.setState({step: this.makeStepTrainingSuccess()}),
+    toStepTranscriptionResults: () => this.setState({step: this.makeStepTranscriptionResults()}),
+    toStepModelSettings: () => this.setState({step: this.makeStepModelSettings()}),
+    toStepTrainingModel: () => this.setState({step: this.makeStepTrainingModel()}),
+  };
+
   makeStepWelcome() {
     return (<StepWelcome
-      toStepNaming={() => this.setState({step: this.makeStepNaming()})}
-      toStepNewTranscription={() => this.setState({step: this.makeStepNewTranscription()})}
+      {... this.stepProps}
       goBack={()=>{}} // no back here
     />);
   }
 
   makeStepNaming() {
     return (<StepNaming
-      toStepAddData={() => this.setState({step: this.makeStepAddData()})}
+      {... this.stepProps}
       goBack={() => this.setState({step: this.makeStepWelcome()})}
     />);
   }
 
   makeStepAddData() {
-    return (<StepAddData 
-    goBack={() => this.setState({step: this.makeStepNaming()})}
+    return (<StepAddData
+      {... this.stepProps}
+      goBack={() => this.setState({step: this.makeStepNaming()})}
     />);
   }
 
   makeStepBuildPronunciationDictionary() {
-    return (<StepBuildPronunciationDictionary />);
+    return (<StepBuildPronunciationDictionary
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepDataPreperation() {
-    return (<StepDataPreperation />);
+    return (<StepDataPreperation
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepNewTranscription() {
-    return (<StepNewTranscription />);
+    return (<StepNewTranscription
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepTrainingSuccess() {
-    return (<StepTrainingSuccess />);
+    return (<StepTrainingSuccess
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepTranscriptionResults() {
-    return (<StepTranscriptionResults />);
+    return (<StepTranscriptionResults
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepModelSettings() {
-    return (<StepModelSettings />);
+    return (<StepModelSettings
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   makeStepTrainingModel() {
-    return (<StepTrainingModel />);
+    return (<StepTrainingModel
+      {... this.stepProps}
+      goBack={()=>{}} // no back here
+    />);
   }
 
   render() {
