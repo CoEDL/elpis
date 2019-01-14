1aa78d07918b7b11368848bfb5bf25a2e3b06772
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 11:25:47 +1000

    🎙 Added first three steps

---
+++ elpis-gui/src/components/StepAddData.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepAddData extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div></div>;
+    }
+}
---
+++ elpis-gui/src/components/StepNaming.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepNaming extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div></div>;
+    }
+}
