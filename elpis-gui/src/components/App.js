ef7e859741be59ead57ce6d66aba83e68653dc62
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 10:53:08 +1000

    🍭 Added a page container

--- elpis-gui/src/components/App.js
+++ elpis-gui/src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import ExampleButton from './Example';
+import PageContainer from './PageContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ExampleButton />
+        <PageContainer body="This is the page body." />
       </div>
     );
   }
