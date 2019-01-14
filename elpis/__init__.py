dced627cc7f10b8b530c6e6bfe1aca3a719ee972
nicklambourne <ndl93@live.com>
2019-01-15 00:21:53 +1000

    flask boilerplate

--- elpis/__init__.py
+++ elpis/__init__.py
@@ -1,10 +1,33 @@
 import os
-os.environ["FLASK_ENV"] = "development"
 
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    with open("elpis/templates/example.tpl.html", "r") as fout:
-        return fout.read()
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__,
+                instance_relative_config=True,
+                static_folder='static')
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        return 'Yo momma'
+
+    return app
