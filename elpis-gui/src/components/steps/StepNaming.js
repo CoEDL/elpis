4dc71bffdcd603b1453ecb8f5c5fd0c6e7c68c34
Emmanuel <etopeojo@gmail.com>
2019-01-14 12:53:19 +1000

    Started Buildup of StepNaming

--- elpis-gui/src/components/steps/StepNaming.js
+++ elpis-gui/src/components/steps/StepNaming.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import AccordionFluid from '../SemanticsComponents/AccordionFluid'
+import { Grid, GridColumn, Placeholder } from 'semantic-ui-react';
 
 export default class StepNaming extends Component {
     // constructor(props) {
     //     super(props);
     // }
     render() {
-        return <div></div>;
+        return(
+            <Grid>
+                <Grid.Column width={4}>
+                    <AccordionFluid title={'Step 1'}/>
+                    <AccordionFluid title={'Step 2'}/>
+                    <AccordionFluid title={'Step 3'}/>
+                </Grid.Column>
+
+                <Grid.Column width={12}>
+                    <Placeholder>
+                        <Placeholder.Header image>
+                            <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+                </Grid.Column>
+            </Grid>
+        )
     }
 }
