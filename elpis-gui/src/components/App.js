a56612f4fdc77dfebbef98dae250af2be36856ab
Emmanuel <etopeojo@gmail.com>
2019-01-14 12:22:05 +1000

    Created Welcome Page component

--- elpis-gui/src/components/App.js
+++ elpis-gui/src/components/App.js
@@ -3,13 +3,15 @@ import logo from '../logo.svg';
 import './App.css';
 import ExampleButton from './Example';
 import PageContainer from './PageContainer'
+import WelcomePage from './WelcomePage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ExampleButton />
-        <PageContainer body="This is the page body." />
+        {/* <ExampleButton />
+        <PageContainer body="This is the page body." /> */}
+        <WelcomePage />
       </div>
     );
   }
---
+++ elpis-gui/src/components/SemanticsComponents/AccordionFluid.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Accordion, Icon } from 'semantic-ui-react'
+
+export default class AccordionExampleFluid extends Component {
+  state = { activeIndex: null }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    const { activeIndex } = this.state
+
+    return (
+      <Accordion styled>
+        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          {this.props.title}
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>
+            A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can
+            be found as a welcome guest in many households across the world.
+          </p>
+        </Accordion.Content>
+      </Accordion>
+    )
+  }
+}
