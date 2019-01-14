41602eecd3641c7f05efc20d3206282230a46972
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-14 11:28:52 +1000

    1️⃣ Added all sub steps for main step 1

---
+++ elpis-gui/src/components/StepBuildPronunciationDictionary.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepBuildPronunciationDictionary extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div></div>;
+    }
+}
---
+++ elpis-gui/src/components/StepDataPreperation.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class StepDataPreperation extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div></div>;
+    }
+}
