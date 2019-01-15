1cb521dfeef1a8942f9dfa87562341be04c6043a
Emmanuel <etopeojo@gmail.com>
2019-01-15 10:55:30 +1000

    StepTrainingModel mockup completed

--- elpis-gui/src/components/App.js
+++ elpis-gui/src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     super(props);
     this.state = {
       // step: this.makeStepWelcome()
-      step: this.makeStepModelSettings()
+      step: this.makeStepTrainingModel()
     };
   }
 
--- elpis-gui/src/components/steps/StepModelSettings.js
+++ elpis-gui/src/components/steps/StepModelSettings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AccordionFluid from '../SemanticsComponents/AccordionFluid'
-import { Grid, Header, Segment, Icon, Form, Button, label } from 'semantic-ui-react';
+import { Grid, Header, Segment, Icon, Form, Button } from 'semantic-ui-react';
 
 export default class StepModelSettings extends Component {
     
