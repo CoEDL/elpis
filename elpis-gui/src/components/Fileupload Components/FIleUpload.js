7736eb8cc04a7e4fdb63674540114437cbc0b93e
Emmanuel <etopeojo@gmail.com>
2019-01-25 13:18:17 +1000

    Tried using superagent for file upload to server from FileUpload Component

--- elpis-gui/src/components/Fileupload Components/FIleUpload.js
+++ elpis-gui/src/components/Fileupload Components/FIleUpload.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classNames from "classnames";
 import Dropzone from "react-dropzone";
 import {fromEvent} from "file-selector";
+import request from "superagent";
 
 export default class FileUpload extends Component {
     state={
@@ -11,8 +12,17 @@ export default class FileUpload extends Component {
 
     onDrop = (acceptedFiles, rejectedFiles) => {
         console.log(acceptedFiles);
+        
+        
+        acceptedFiles.forEach(file => {
+            //console.log(file)
+            request
+            .post('http://127.0.0.1:5000/corpus/wav')
+            .attach(file.name, file.path);
+        });
+
         const fileNames = acceptedFiles.map(f => f.name);
-        console.log(fileNames);
+        //console.log(fileNames);
         this.setState({ ...this.state, files: acceptedFiles, fileNames: fileNames  });
     };
 
@@ -21,29 +31,34 @@ export default class FileUpload extends Component {
 
         return(
             <div className="App">
-                <Dropzone className="dropzone"  onDrop={this.onDrop} getDataTransferItems={evt => fromEvent(evt)}>
-                    {({ getRootProps, getInputProps, isDragActive }) => {
-                        return (
-                            <div
-                                {...getRootProps()}
-                                className={classNames("dropzone", {
-                                    "dropzone_active": isDragActive
-                                })}
-                            >
-                                <input {...getInputProps()} />
-                                    {isDragActive ? (
-                                        <p>Drop files here...</p>
-                                    ) : (
-                                        <p>
-                                            Drop individual files or a folder containing your file here
-                                        </p>
-                                    )}
+                    <Dropzone className="dropzone"  onDrop={this.onDrop} getDataTransferItems={evt => fromEvent(evt)}>
+                        {({ getRootProps, getInputProps, isDragActive }) => {
+                            return (
+                                <div
+                                    {...getRootProps()}
+                                    className={classNames("dropzone", {
+                                        "dropzone_active": isDragActive
+                                    })}
+                                >
+                                
+                                    <input {...getInputProps()} />
+                                
+                                        {isDragActive ? (
+                                            <p>Drop files here...</p>
+                                        ) : (
+                                            <p>
+                                                Drop individual files or a folder containing your file here
+                                            </p>
+                                        )}
+                            
                             </div>
                         );
                     }}
-                </Dropzone>
-                <ul>{fileNameList}</ul>
+                    </Dropzone>
+
+                    <ul>{fileNameList}</ul>
             </div>
+                
         );
     }
 }
