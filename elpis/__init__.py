cf3bc45a3d4a3c339c0bb118ce4da59ff46a27ba
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-29 14:17:38 +1000

    📈 Can now access GUI react routes directly

--- elpis/__init__.py
+++ elpis/__init__.py
@@ -8,7 +8,7 @@ from . import corpus
 ELPIS_ROOT_DIR = os.getcwd()
 GUI_ROOT_DIR = os.path.join(ELPIS_ROOT_DIR, "elpis-gui/build")
 GUI_PUBLIC_DIR = GUI_ROOT_DIR
-GUI_STATIC_DIR = os.path.join(GUI_ROOT_DIR, "static")
+GUI_STATIC_DIR = os.path.join(GUI_ROOT_DIR, "js")
 
 def create_app(test_config=None):
     print(os.getcwd())
@@ -17,16 +17,18 @@ def create_app(test_config=None):
     app = Flask(__name__,
                 instance_relative_config=True,
                 static_folder=GUI_STATIC_DIR,
-                static_url_path='/static')
+                static_url_path='/js')
 
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
     app.register_blueprint(corpus.bp)
 
-    @app.route('/')
-    @app.route('/index.html')
-    def index():
+    @app.route('/', defaults={'path':''})
+    @app.route('/index.html', defaults={'path':''})
+    @app.route("/<path:path>")
+    def index(path):
+        print('in index with:', path)
         with open(f"{GUI_PUBLIC_DIR}/index.html", "r") as fin:
             content = fin.read()
             return content
