7519c76f258ddb92ad77bff86af71116d82cd870
Nicholas Buckeridge <bucknich@gmail.com>
2019-01-29 12:18:05 +1000

    🤔 Added reduc thunk

--- elpis-gui/src/redux/store.js
+++ elpis-gui/src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
-
+import thunk from 'redux-thunk'
 import rootReducer from './reducer'
 
 const persistConfig = {
@@ -10,5 +11,8 @@ const persistConfig = {
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
-export const store = createStore(persistedReducer)
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 export const persistor = persistStore(store)
