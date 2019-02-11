ef97618f70385b4d97383606cfbab04de135435e
Nicholas Buckeridge <bucknich@gmail.com>
2019-02-11 14:23:11 +1000

    🎛 Added some tests

--- test.sh
+++ test.sh
@@ -3,9 +3,13 @@
 # Exit with fail if any command in this script fails
 set -e
 
-# Test python project
-# source venv/bin/activate
-# export PYTHONPATH=`pwd`/..
+cd /elpis
+apt-get install python3-venv
+python3 -m venv venv_test
+source venv_test/bin/activate
+pip install pytest
+pip install -r requirements.txt
+alias pytest=venv_test/bin/pytest
 pytest
 
 # Test React and Javascript code
