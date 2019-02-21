a1f62cd4f7f7aeeaf601910148e0758780b15f31
nicklambourne <ndl93@live.com>
2019-02-21 17:06:08 +1000

    conflict this

--- elpis/__init__.py
+++ elpis/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import redirect
 from . import api
 from .app import Flask
@@ -9,8 +7,8 @@ from .paths import GUI_STATIC_DIR, GUI_PUBLIC_DIR
 from .kaldi.interface import KaldiInterface
 from pathlib import Path
 
-def create_app(test_config=None):
 
+def create_app(test_config=None):
     # Setup static resources
     # create and configure the app
     app = Flask(__name__,
@@ -28,24 +26,23 @@ def create_app(test_config=None):
         app.config['INTERFACE'] = KaldiInterface.load(interface_path)
     app.config['CURRENT_DATABUNDLE'] = None
     app.config['CURRENT_MODEL'] = None
-    
+
     app.register_blueprint(api.bp)
     print(app.url_map)
-
 
     @app.route('/index.html')
     def index_file():
         """Redirects to '/' for React."""
         return redirect('/')
 
-    @app.route('/', defaults={'path':''})
+    @app.route('/', defaults={'path': ''})
     @app.route("/<path:path>")
     def index(path):
         print('in index with:', path)
         with open(f"{GUI_PUBLIC_DIR}/index.html", "r") as fin:
             content = fin.read()
             return content
-    
+
     @app.route('/favicon.ico')
     def favicon():
         with open(f"{GUI_PUBLIC_DIR}/favicon.ico", "rb") as fin:
