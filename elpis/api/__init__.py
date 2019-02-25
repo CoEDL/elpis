53b9716015532000bb58f21338789171695f575a
Nicholas Buckeridge <bucknich@gmail.com>
2019-02-25 17:42:06 +1000

    🗣Exposed log file

--- elpis/api/__init__.py
+++ elpis/api/__init__.py
@@ -3,6 +3,8 @@ from . import databundle
 from . import model
 from . import transcription
 
+from pathlib import Path
+
 bp = Blueprint("api", __name__, url_prefix="/api")
 bp.register_blueprint(databundle.bp)
 bp.register_blueprint(model.bp)
@@ -12,3 +14,12 @@ bp.register_blueprint(transcription.bp)
 def whole_state():
     # TODO: implement this if needed
     return '{"yet to be named": "the enture model!"}'
+
+@bp.route("/log.txt")
+def log():
+    log_file = Path('/elpis/elpis/state/tmp_log.txt')
+    if log_file.exists():
+        with log_file.open() as fin:
+            return fin.read()
+    else:
+        return "No log file."
