02b6ae1fff49d17a0eda6d8fe4d98cb52a311f5d
Ben Foley <ben@cbmm.io>
2019-09-19 11:17:27 +0200

    mention the new step on the welcome page

--- elpis-gui/src/components/Steps/Welcome.js
+++ elpis-gui/src/components/Steps/Welcome.js
@@ -32,12 +32,13 @@ class StepWelcome extends Component {
 
 							<p>
 								Elpis is a speech recognition tool, which you can use to transcribe audio.
-								It works in three parts:
+								It works in four parts:
 							</p>
 							<p>
 								Step 1) Add some language recordings and their transcriptions that you already have <br />
-								Step 2) Training the tool <br />
-								Step 3) Use it to tanscribe new audio
+								Step 2) Creating a pronunciation dictionary <br />
+								Step 3) Training the speech recognition "models" <br />
+								Step 4) Use it to tanscribe new audio
 							</p>
 
 						</Segment>
