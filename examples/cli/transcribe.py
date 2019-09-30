0c55154b4c3fb1b5c3c1bef552c599bbe99f09c9
Ben Foley <ben@cbmm.io>
2019-10-01 05:57:16 +1000

    Change cook generate infer files to python

--- examples/cli/transcribe.py
+++ examples/cli/transcribe.py
@@ -6,12 +6,16 @@ from pathlib import Path
 kaldi = KaldiInterface('/elpis/state')
 m = kaldi.get_model('mx')
 
+
 # Make a transcription interface and transcribe unseen audio to elan.
-t = kaldi.new_transcription('tx')
-t.link(m)
-with open('/recordings/untranscribed/audio.wav', 'rb') as faudio:
-    t.prepare_audio(faudio)
+# t = kaldi.new_transcription('tx')
+# t.link(m)
 # t.transcribe_align()
 # print(t.elan().decode('utf-8'))
+
+# Use existing transcription
+t = kaldi.get_transcription('tx')
+with open('/recordings/untranscribed/audio.wav', 'rb') as faudio:
+    t.prepare_audio(faudio)
 t.transcribe()
 print(t.text().decode('utf-8'))
