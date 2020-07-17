cc2ffaf933f8c07ff548afe633b8857ba37996ac
Nicholas Buckeridge <bucknich@gmail.com>
2020-07-17 01:29:39 +0000

    cli_with_elan.py works with existing data

--- examples/cli/cli_with_elan.py
+++ examples/cli/cli_with_elan.py
@@ -10,22 +10,19 @@ from elpis.engines.common.objects.interface import Interface
 # ======
 # Create a Kaldi interface directory (where all the associated files/objects
 # will be stored).
-kaldi = Interface(path='/state', use_existing=True)
+elpis = Interface(path='/state', use_existing=True)
 
 # Step 1
 # ======
 # Setup a dataset to to train data on.
-ds = kaldi.new_dataset('dsy')
-ds.add_directory('/recordings/transcribed', extensions=['eaf', 'wav'])
-ds.auto_select_importer()
-# ds.import_with('Elan').import_directory('/recordings/transcribed')
-ds.importer.set_setting('tier_name', 'Phrase')
-
-# # Change an importing setting
-# ds.importer().change_tier('Phrase')
-
-
-ds.process()
+if 'dsy' not in elpis.list_datasets():
+    ds = elpis.new_dataset('dsy')
+    ds.add_directory('/recordings/transcribed', extensions=['eaf', 'wav'])
+    ds.auto_select_importer() # Selects Elan because of eaf file.
+    ds.importer.set_setting('tier_name', 'Phrase')
+    ds.process()
+else:
+    ds = elpis.get_dataset('dsy')
 
 # Step 2
 # ======
@@ -33,30 +30,39 @@ ds.process()
 from elpis.engines import ENGINES
 engine_name = 'kaldi'
 engine = ENGINES[engine_name]
-kaldi.set_engine(engine)
+elpis.set_engine(engine)
 
 # Step 3
 # ======
 # Build pronunciation dictionary
-pd = kaldi.new_pron_dict('pd')
-pd.link(ds)
-pd.set_l2s_path('/recordings/letter_to_sound.txt')
-pd.generate_lexicon()
+if 'pd' not in elpis.list_pron_dicts():
+    pd = elpis.new_pron_dict('pd')
+    pd.link(ds)
+    pd.set_l2s_path('/recordings/letter_to_sound.txt')
+    pd.generate_lexicon()
+else:
+    pd = elpis.get_pron_dict('pd')
 
 # Step 4
 # ======
 # Link dataset and pd to a new model, then train the model.
-m = kaldi.new_model('mx')
-m.link(ds, pd)
-m.build_kaldi_structure()
-m.train() # may take a while
+if 'mx' not in elpis.list_models():
+    m = elpis.new_model('mx')
+    m.link(ds, pd)
+    m.build_kaldi_structure()
+    m.train() # may take a while
+else:
+    m = elpis.get_model('mx')
 
 # Step 5
 # ======
 # Make a transcription interface and transcribe unseen audio to elan.
-t = kaldi.new_transcription('tx')
-t.link(m)
-with open('/recordings/untranscribed/audio.wav', 'rb') as faudio:
-    t.prepare_audio(faudio)
-t.transcribe()
+if 'tx' not in elpis.list_transcriptions():
+    t = elpis.new_transcription('tx')
+    t.link(m)
+    with open('/recordings/untranscribed/audio.wav', 'rb') as faudio:
+        t.prepare_audio(faudio)
+    t.transcribe()
+else:
+    t = elpis.get_transcription('tx')
 print(t.text())
