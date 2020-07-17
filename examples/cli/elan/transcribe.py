ebe130a512dcfc7df7d2d3a7f125de4027da2e00
Ben Foley <ben@cbmm.io>
2020-07-17 12:23:35 +1000

    change demo object name (#89)

--- examples/cli/elan/transcribe.py
+++ examples/cli/elan/transcribe.py
@@ -4,7 +4,7 @@ from elpis.engines.common.objects.interface import Interface
 # ======
 # Create a Kaldi interface directory (where all the associated files/objects
 # will be stored).
-kaldi = Interface(path='state', use_existing=True)
+elpis = Interface(path='state', use_existing=True)
 
 # Step 1
 # ======
@@ -12,17 +12,17 @@ kaldi = Interface(path='state', use_existing=True)
 from elpis.engines import ENGINES
 engine_name = 'kaldi'
 engine = ENGINES[engine_name]
-kaldi.set_engine(engine)
+elpis.set_engine(engine)
 
 # Step 2
 # ======
 # Load Model
-m = kaldi.get_model('mx')
+m = elpis.get_model('mx')
 
 # Step 3
 # ======
 # Make a transcription interface and transcribe unseen audio to elan.
-t = kaldi.new_transcription('tx')
+t = elpis.new_transcription('tx')
 t.link(m)
 with open('/recordings/untranscribed/audio.wav', 'rb') as faudio:
     t.prepare_audio(faudio)
