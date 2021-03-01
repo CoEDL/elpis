a43c2c13c927b4578d24e9e8b403ef7f74543778
Ben Foley <ben@cbmm.io>
2021-03-01 22:36:42 +1000

    wip transcription choice landing page

--- elpis/gui/src/components/Transcription/ChooseTranscriptionModel.js
+++ elpis/gui/src/components/Transcription/ChooseTranscriptionModel.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import { Grid, Button, Header, Container, Segment } from 'semantic-ui-react';
+import { modelLoad, modelList } from 'redux/actions/modelActions';
 
 
 class ChooseTranscriptionModel extends Component {
 
-    render() {
+    componentDidMount() {
+        this.props.modelList()
+    }
 
-    return (
-        <pre>
-            Choose a model:
-                import
-                use existing
-                train a new model
-        </pre>
+
+    render() {
+        const { t, currentEngine, list } = this.props
+        console.log("list", list)
+
+        const modelList = list.map((model, index) => (<p key={index}>{model.name}</p>))
+        return (
+            <pre>
+                Choose a model:
+                    <Segment>
+                        import ( coming soon )
+                    </Segment>
+                    <Segment>
+                        use existing
+                        ( list of models )
+
+                        {modelList}
+
+                    </Segment>
+                    <Segment>
+                        <Link to={urls.gui.engine.index}>train a new model</Link>
+                    </Segment>
+            </pre>
         )
     }
 }
 
 const mapStateToProps = state => {
     return {
+        list: state.model.modelList,
+        currentEngine: state.engine.engine
     }
 }
 
 const mapDispatchToProps = dispatch => ({
+    modelList: () => {
+        dispatch(modelList())
+    },
 })
 
 export default
