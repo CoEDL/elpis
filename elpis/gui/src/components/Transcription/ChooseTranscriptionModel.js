fd9a118450af66363654e37cecf5cfd175b5f5e5
Ben Foley <ben@cbmm.io>
2021-03-02 09:36:26 +1000

    wip page to choose transcription model

--- elpis/gui/src/components/Transcription/ChooseTranscriptionModel.js
+++ elpis/gui/src/components/Transcription/ChooseTranscriptionModel.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import { Grid, Button, Header, Container, Segment } from 'semantic-ui-react';
 import { modelLoad, modelList } from 'redux/actions/modelActions';
+import { datasetLoad } from 'redux/actions/datasetActions';
+import { pronDictLoad } from 'redux/actions/pronDictActions';
+import urls from 'urls'
 
 
 class ChooseTranscriptionModel extends Component {
@@ -12,12 +15,31 @@ class ChooseTranscriptionModel extends Component {
         this.props.modelList()
     }
 
+    handleSelectModel = (model_name) => {
+    console.log("model_name", model_name)
+        const { list, modelLoad } = this.props
+        // get the matching ds and pd values
+        var selectedModel = list.filter(m => m.name==model_name)
+        // argh, this is weird, but reusing code from Model Dashboard
+        const modelData = { name: selectedModel[0].name }
+        const datasetData = { name: selectedModel[0].dataset_name }
+        const pronDictData = { name: selectedModel[0].pron_dict_name }
+        modelLoad(modelData, datasetData, pronDictData)
+    }
+
 
     render() {
         const { t, currentEngine, list } = this.props
         console.log("list", list)
 
-        const modelList = list.map((model, index) => (<p key={index}>{model.name}</p>))
+        const modelList = list.map((model, index) => {
+            return (
+                <Button key={index} onClick={() => this.handleSelectModel(model.name)}>
+                    {model.name}
+                </Button>
+            )
+        })
+
         return (
             <pre>
                 Choose a model:
@@ -50,6 +72,11 @@ const mapDispatchToProps = dispatch => ({
     modelList: () => {
         dispatch(modelList())
     },
+    modelLoad: (modelData, datasetData, pronDictData) => {
+        dispatch(modelLoad(modelData))
+            .then(response => dispatch(datasetLoad(datasetData)))
+            .then(response => dispatch(pronDictLoad(pronDictData)))
+    }
 })
 
 export default
