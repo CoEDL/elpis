9d0417d6877cb89cdd3e1dd04957aa980a5c42e5
mattchrlw <mattchrlw@gmail.com>
2021-03-24 09:48:55 +1000

    Oops forgot to run it

--- elpis/gui/src/index.js
+++ elpis/gui/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-
 import {Provider} from "react-redux";
 import store from "./redux/store";
-
 import "./i18n";
 
 
