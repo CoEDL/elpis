42c697363bb89473d7f0af68e9cc9e659a5eced4
mattchrlw <mattchrlw@gmail.com>
2021-04-22 22:21:31 +1000

    ES6 and other stuff

--- elpis/gui/src/components/Dataset/FileUpload.js
+++ elpis/gui/src/components/Dataset/FileUpload.js
@@ -1,47 +1,61 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import classNames from "classnames";
 import Dropzone from "react-dropzone";
-import {Button} from "semantic-ui-react";
+import {Button, Message, MessageHeader} from "semantic-ui-react";
 import {fromEvent} from "file-selector";
 import {withTranslation} from "react-i18next";
 import {datasetFiles} from "redux/actions/datasetActions";
 import {connect} from "react-redux";
 
 class FileUpload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {missingFiles: []};
+    }
+
     parseElan = async (file) => {
         return new Promise((resolve, reject) => {
-            const reader = new FileReader();
+            const reader = new window.FileReader();
+
             reader.readAsText(file);
             reader.onload = () => {
-                const parser = new DOMParser();
-                const eafDoc = parser.parseFromString(reader.result, "application/xml")
+                const parser = new window.DOMParser();
+                const eafDoc = parser.parseFromString(reader.result, "application/xml");
                 const wavUrl = eafDoc
                     .getElementsByTagName("ANNOTATION_DOCUMENT")[0]
                     .getElementsByTagName("HEADER")[0]
                     .getElementsByTagName("MEDIA_DESCRIPTOR")[0]
                     .getAttribute("RELATIVE_MEDIA_URL").split("./")[1];
+
                 resolve(wavUrl);
-            }
+            };
             reader.onerror = () => {
                 reject(reader.error);
-            }
-        })
+            };
+        });
     }
 
     onDrop = async (acceptedFiles) => {
+        // TODO: Behaviour when dropping multiple times is still undefined
+        // (do we empty the dataset?)
+        this.setState({missingFiles: []});
         console.log("files dropped:", acceptedFiles);
+
         const eafFiles = acceptedFiles
-            .filter(file => file.name.split('.').pop() === "eaf");
+            .filter(file => file.name.split(".").pop() === "eaf");
         const wavFileNames = acceptedFiles
-            .filter(file => file.name.split('.').pop() === "wav")
+            .filter(file => file.name.split(".").pop() === "wav")
             .map(file => file.name);
 
         // for each is not supported with await for some reason...
         for (let i = 0; i < eafFiles.length; i++) {
             const parsedWavFile = await this.parseElan(eafFiles[i]);
-            const identicalWavFile = eafFiles[i].name.split('.')[0].concat(".wav");
+            const identicalWavFile = eafFiles[i].name.split(".")[0].concat(".wav");
+
             if (!wavFileNames.includes(parsedWavFile) && !wavFileNames.includes(identicalWavFile)) {
-                console.log("Missing wav file, provide either", identicalWavFile, "or", parsedWavFile);
+                this.setState(prevState => ({
+                    missingFiles: [...prevState.missingFiles, [identicalWavFile, parsedWavFile]],
+                }));
             }
         }
 
@@ -59,6 +73,24 @@ class FileUpload extends Component {
 
         return (
             <div className="FileUpload">
+                {this.state.missingFiles.length > 0 && 
+                    <Message negative>
+                        <MessageHeader>
+                            {t("dataset.files.missingAudioFiles")}
+                        </MessageHeader>
+                        <p>
+                            {t("dataset.files.missingAudioFilesDescription")}
+                        </p>
+                        <ul>
+                            {this.state.missingFiles.map(wavFile => {
+                        return (
+                            <li key={wavFile[0]}>
+                                {wavFile[0]} / {wavFile[1]}
+                            </li>
+);
+                    })}
+                        </ul>
+                    </Message>}
                 <Dropzone
                     disabled={interactionDisabled}
                     className="dropzone"
