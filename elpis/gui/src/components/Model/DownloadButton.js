86784618cf91bbd4fa041c47e5d882003c849ed4
Ben Foley <ben@cbmm.io>
2022-10-12 11:27:02 +1000

    Use downloadjs to trigger model zip download (#320)

--- elpis/gui/src/components/Model/DownloadButton.js
+++ elpis/gui/src/components/Model/DownloadButton.js
@@ -1,12 +1,17 @@
 import React, {useState} from "react";
 import {Button, Loader, Segment} from "semantic-ui-react";
 import urls from "urls";
+import downloadjs from "downloadjs";
 
 export default function DownloadButton() {
   const [downloading, setDownloading] = useState(false);
   const download_model = async () => {
     setDownloading(true);
-    await fetch(urls.api.model.download);
+
+    let response = await fetch(urls.api.model.download);
+    let blob = await response.blob();
+
+    downloadjs(blob, "model.zip", "application/zip");
     setDownloading(false);
   };
 
